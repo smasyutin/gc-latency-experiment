92f526ba944797c834e42b361cee544148d8cb48
smasyutin <serg.masyutin@gmail.com>
2020-04-19 19:26:00 -0700

    - update to the latest versions in Dockerfiles - update to measure not just max time, but a distribution - added warm-up phase - added `message count` and `window size` as input parameters

--- go/main.go
+++ go/main.go
@@ -2,42 +2,110 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
 const (
-	windowSize = 200000
-	msgCount   = 1000000
+	warmUpBatchSize = 10_000
+	msgSize         = 1024
 )
 
 type message []byte
-
-type channel [windowSize]message
-
-var worst time.Duration
+type channel []message
 
 func mkMessage(n int) message {
-	m := make(message, 1024)
+	m := make(message, msgSize)
 	for i := range m {
 		m[i] = byte(n)
 	}
 	return m
 }
 
-func pushMsg(c *channel, highID int) {
+func pushMsg(c *channel, id int) time.Duration {
 	start := time.Now()
-	m := mkMessage(highID)
-	(*c)[highID%windowSize] = m
-	elapsed := time.Since(start)
-	if elapsed > worst {
-		worst = elapsed
+	m := mkMessage(id)
+	(*c)[id] = m
+	return time.Since(start)
+}
+
+func runBatch(msgCount int, windowSize int) {
+	fmt.Println("Message count:", msgCount)
+	fmt.Println("Bugger size: ", windowSize)
+
+	var stats = Stats{msgCount: msgCount}
+
+	start := time.Now()
+	c := make(channel, windowSize)
+	allocated := time.Since(start)
+	for i := 0; i < msgCount; i++ {
+		stats.record(
+			pushMsg(&c, i%windowSize))
 	}
+	elapsed := time.Since(start)
+	fmt.Println("Allocation time:", allocated.String())
+	fmt.Println("Push time stats:\n", stats.String())
+	fmt.Println("Total time:", elapsed.Seconds(), "s")
+}
+
+func warmUp(windowSize int) {
+	fmt.Println("Warming-up...")
+	runBatch(warmUpBatchSize, windowSize)
+	fmt.Println("...Warmed-up")
 }
 
 func main() {
-	var c channel
-	for i := 0; i < msgCount; i++ {
-		pushMsg(&c, i)
+	var msgCount, _ = strconv.Atoi(os.Args[1])
+	var windowSize, _ = strconv.Atoi(os.Args[2])
+	warmUp(windowSize)
+	runBatch(msgCount, windowSize)
+}
+
+type Stats struct {
+	msgCount int
+	max      time.Duration
+	// histograms of values for each of `i microsecond`
+	// up to 500_000microseconds = 0.5s, should be enough for GC latency
+	histogram [500_000]int
+}
+
+func (s *Stats) record(t time.Duration) {
+	if t > s.max {
+		s.max = t
 	}
-	fmt.Println("Worst push time: ", worst)
+	index := int(t.Microseconds())
+	if index >= len(s.histogram) {
+		index = len(s.histogram) - 1
+	}
+	s.histogram[index]++
+}
+
+func (s *Stats) String() string {
+	return "max: " + s.max.String() +
+		"\n68.3 percentile: " + s.percentile(0.683).String() +
+		"\n95.4 percentile: " + s.percentile(0.954).String() +
+		"\n99.7 percentile: " + s.percentile(0.997).String() +
+		"\n" + s.countLongerThanMs(10) + " longer than 10ms" +
+		"\n" + s.countLongerThanMs(30) + " longer than 30ms" +
+		"\n" + s.countLongerThanMs(100) + " longer than 100ms"
+}
+
+func (s *Stats) percentile(percent float64) time.Duration {
+	valuesCount := int(percent * float64(s.msgCount))
+	index := 0
+	for n := 0; (index < len(s.histogram)) && (n < valuesCount); index++ {
+		n += s.histogram[index]
+	}
+
+	return time.Duration(index) * time.Microsecond
+}
+
+func (s *Stats) countLongerThanMs(ms int) string {
+	result := 0
+	for i := ms * 1000; i < len(s.histogram); i++ {
+		result += s.histogram[i]
+	}
+
+	return strconv.Itoa(result)
 }
--- java/Main.java
+++ java/Main.java
@@ -1,13 +1,11 @@
 import java.util.Arrays;
-import java.util.HashMap;
 
 public class Main {
 
-    private static final int windowSize = 200_000;
-    private static final int msgCount = 1_000_000;
+    // batch size for JVM taken from this article
+    // https://noelwelsh.com/posts/2020-02-13-benchmarking-graal-native-image.html
+    private static final int warmUpBatchSize = 10_000;
     private static final int msgSize = 1024;
-
-    private static long worst = 0;
 
     private static byte[] createMessage(final int n) {
         final byte[] msg = new byte[msgSize];
@@ -15,20 +13,93 @@ public class Main {
         return msg;
     }
 
-    private static void pushMessage(final byte[][] store, final int id) {
+    private static long pushMessage(final byte[][] c, final int id) {
         final long start = System.nanoTime();
-        store[id % windowSize] = createMessage(id);
-        final long elapsed = System.nanoTime() - start;
-        if (elapsed > worst) {
-            worst = elapsed;
+        byte[] m = createMessage(id);
+        c[id] = m;
+        return System.nanoTime() - start;
+    }
+
+    private static void runBatch(int msgCount, int windowSize) {
+        System.out.println("Message count: " + msgCount);
+        System.out.println("Bugger size: " + windowSize);
+        final Stats stats = new Stats(msgCount);
+
+        final long start = System.nanoTime();
+        final byte[][] c = new byte[windowSize][msgSize];
+        final long allocated = System.nanoTime() - start;
+        for (int i = 0; i < msgCount; i++) {
+            stats.record(
+                    pushMessage(c, i % windowSize)
+            );
         }
+        System.out.println("Allocation time: " + (allocated / 1000) + "µs");
+        final long elapsed = System.nanoTime() - start;
+        System.out.println("Push time stats:\n" + stats);
+        System.out.println("Total time: " + ((float) elapsed / 1_000_000_000) + "s");
+    }
+
+    private static void warmUp(int windowSize) {
+        System.out.println("Warming-up...");
+        runBatch(warmUpBatchSize, windowSize);
+        System.out.println("...Warmed-up");
     }
 
     public static void main(String[] args) {
-        final byte[][] store = new byte[windowSize][msgSize];
-        for (int i = 0; i < msgCount; i++) {
-            pushMessage(store, i);
+        final int msgCount = Integer.parseInt(args[0]);
+        final int windowSize = Integer.parseInt(args[1]);
+
+        warmUp(windowSize);
+        runBatch(msgCount, windowSize);
+    }
+
+    private static class Stats {
+        int msgCount;
+        long max = 0;
+        long[] histogram = new long[500_000];
+
+        public Stats(int msgCount) {
+            this.msgCount = msgCount;
         }
-        System.out.println("Worst push time: " + (worst / 1000_000));
+
+        void record(long tNano) {
+            int tMicro = (int)(tNano / 1000);
+            if (tMicro > max) {
+                max = tMicro;
+            }
+            int index = tMicro;
+            if (index >= histogram.length) {
+                index = histogram.length -1;
+            }
+            histogram[index]++;
+        }
+
+        long percentile(double percent) {
+            int valuesCount = (int) (msgCount * percent);
+            int index = 0;
+            for (int n = 0; (index < histogram.length) && (n < valuesCount); index++) {
+                n += histogram[index];
+            }
+
+            return index;
+        }
+
+        Long countLongerThanMs(int ms) {
+            long result = 0;
+            for (int i = (ms * 1000); i < histogram.length; i++) {
+                result += histogram[i];
+            }
+            return result;
+        }
+
+        public String toString() {
+            return "max: " + (max / 1000) + "ms" +
+                    "\n68.3 percentile: " + percentile(0.683) + "µs" +
+                    "\n95.4 percentile: " + percentile(0.954) + "µs" +
+                    "\n99.7 percentile: " + percentile(0.997) + "µs" +
+                    "\n" + countLongerThanMs(10) + " longer than 10ms" +
+                    "\n" + countLongerThanMs(30) + " longer than 30ms" +
+                    "\n" + countLongerThanMs(100) + " longer than 100ms";
+        }
     }
 }
